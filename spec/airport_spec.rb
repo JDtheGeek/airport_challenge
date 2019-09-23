6b3496d72a4ae973b7e159d6a6652ec733067ac1
John Dennehy <john.dennehy@gmail.com>
2019-09-23 18:51:40 +0100

    refactors airport_spec.rb to replace duplicated initialising

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,83 +1,77 @@
 require 'airport'
 
 describe Airport do
+subject(:airport) { described_class.new}
+
   describe '#planes' do
     it 'returns list of planes currently landed' do
-      gatwick = Airport.new
-      allow(gatwick).to receive(:safe_to_fly?).and_return(true)
-      gatwick.update_forecast
+      allow(airport).to receive(:safe_to_fly?).and_return(true)
+      airport.update_forecast
 
       array = []
       5.times {
         plane = Plane.new
-        gatwick.land(plane)
+        airport.land(plane)
         array << plane
       }
-      expect(gatwick.planes).to eq array
+      expect(airport.planes).to eq array
     end
   end
   
   describe "#land" do
     it "instructs plane to land at the airport" do
-      gatwick = Airport.new
-      allow(gatwick).to receive(:safe_to_fly?).and_return(true)
-      gatwick.update_forecast
+      allow(airport).to receive(:safe_to_fly?).and_return(true)
+      airport.update_forecast
       plane_1 = Plane.new
-      expect(gatwick.land(plane_1)).to eq "roger wilko"
+      expect(airport.land(plane_1)).to eq "roger wilko"
     end
     it "prevents landing if airport full" do
-      gatwick = Airport.new
-      allow(gatwick).to receive(:safe_to_fly?).and_return(true)
-      gatwick.update_forecast
-      20.times { gatwick.land(Plane.new) }
+      allow(airport).to receive(:safe_to_fly?).and_return(true)
+      airport.update_forecast
+      20.times { airport.land(Plane.new) }
       my_plane = Plane.new
-      expect { gatwick.land(my_plane) }.to raise_error "At capacity"
+      expect { airport.land(my_plane) }.to raise_error "At capacity"
     end
     it "prevents landing if weather is stormy" do
-      gatwick = Airport.new
       plane_1 = Plane.new
-      allow(gatwick).to receive(:safe_to_fly?).and_return(false)
-      gatwick.update_forecast 
-      expect { gatwick.land(plane_1) }.to raise_error "Permission denied due to weather"
+      allow(airport).to receive(:safe_to_fly?).and_return(false)
+      airport.update_forecast 
+      expect { airport.land(plane_1) }.to raise_error "Permission denied due to weather"
     end
     it 'prevents landing if plane already landed' do
-      gatwick = Airport.new
-      allow(gatwick).to receive(:safe_to_fly?).and_return(true)
-      gatwick.update_forecast
+      allow(airport).to receive(:safe_to_fly?).and_return(true)
+      airport.update_forecast
       my_plane = Plane.new
-      gatwick.land(my_plane)
-      expect { gatwick.land(my_plane) }.to raise_error 'Already landed!'
+      airport.land(my_plane)
+      expect { airport.land(my_plane) }.to raise_error 'Already landed!'
     end
   end
 
   describe "#take_off" do
     it "instructs a plane to take_off and returns confirmation" do
-      gatwick = Airport.new
-      allow(gatwick).to receive(:safe_to_fly?).and_return(true)
-      gatwick.update_forecast
+      allow(airport).to receive(:safe_to_fly?).and_return(true)
+      airport.update_forecast
       plane_1 = Plane.new
-      gatwick.land(plane_1)
-      expect(gatwick.take_off(plane_1)).to eq "In the air"
+      airport.land(plane_1)
+      expect(airport.take_off(plane_1)).to eq "In the air"
     end
 
     it "it raises error if plane not at that airport" do
-      gatwick = Airport.new
-      allow(gatwick).to receive(:safe_to_fly?).and_return(true)
-      gatwick.update_forecast
+      allow(airport).to receive(:safe_to_fly?).and_return(true)
+      airport.update_forecast
       heathrow = Airport.new
       plane_1 = Plane.new
-      gatwick.land(plane_1)
+      airport.land(plane_1)
       expect { heathrow.take_off(plane_1) }.to raise_error "No such plane"
     end
   end
 
   describe '#amend_capacity' do
     it 'allows the default capacity to be over-ridden' do
-      gatwick = Airport.new
-      gatwick.capacity = 10
-      10.times { gatwick.land(Plane.new) }
+      airport.capacity = 10
+      10.times { airport.land(Plane.new) }
       my_plane = Plane.new
-      expect { gatwick.land(my_plane) }.to raise_error('At capacity')
+      expect { airport.land(my_plane) }.to raise_error('At capacity')
     end
   end
 end
